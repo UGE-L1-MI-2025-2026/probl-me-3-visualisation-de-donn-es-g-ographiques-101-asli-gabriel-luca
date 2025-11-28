0b4ee51ce0a7a52674332cfae94940af9beb07f2
GabrielAlvesVerissimo <alvesverissimo@edu.univ-eiffel.fr>
2025-11-28 12:46:13 +0100

    Dimension du département

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import shapefile
 import fltk
-
+import math
 sf = shapefile.Reader("departements-20180101") #ouverture du fichier shapefile
 sf.records() # visualisation de toutes les entrées du fichier
 
@@ -10,10 +10,25 @@ seine_et_marne.bbox # Les points extrémaux de la seine-et-marne
 #print(seine_et_marne.points) # La liste des points du contour de la Seine-et-Marne
 var = seine_et_marne.points
 
+rayon = 5000
+
 for i in range(len(var)):
-    var[i] = (var[i][0]*100, 600 - var[i][1]*100 + 4500)
+    X = var[i][0]*(math.pi/180)
+    Y = var[i][1]*(math.pi/180)
+    
+    x = rayon * X
+    y = rayon * ( math.log( math.tan( ( math.pi/4)+( Y/2) ) ) )
+    y_max = rayon * ( math.log( math.tan( ( math.pi/4)+(44) ) ) )
+    
+    x = x-seine_et_marne.bbox[0]
+    y = y_max - y
+    
+    var[i] = (x, y)
+print(var)
+
 
 fltk.cree_fenetre(800, 600)
 fltk.polygone(var,tag="seine")
+fltk.deplace('seine', 0, 4800)
 fltk.attend_ev()
 fltk.ferme_fenetre()
