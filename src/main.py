bde22aabc2658d2f5e233d54c6e6230377c7bafe
Luca Pham-Trong <luca.pham-trong@edu.univ-eiffel.fr>
2025-12-01 12:05:29 +0100

    code nettoyé mergé

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ ROOT_DIR = path.dirname(path.dirname(__file__))
 ASSETS_DIR = path.join(ROOT_DIR, 'assets')
 MAP_DATA_DIR = path.join(ASSETS_DIR, 'map_data')
 IMG_DIR = path.join(ASSETS_DIR, 'images')
-RAYON = 5000
+RAYON = 2300
 y_max = RAYON * math.log(math.tan(math.pi / 4 + 44))
 
 SCALE = 100
@@ -22,20 +22,29 @@ sf.records()  # visualisation de toutes les entrées du fichier
 seine_et_marne = sf.shape(47)
 pts_seine_et_marne = seine_et_marne.points
 
-for i in range(len(pts_seine_et_marne)):
-    lmbd = pts_seine_et_marne[i][0] * (math.pi / 180)  # lambda
-    phi = pts_seine_et_marne[i][1] * (math.pi / 180)  # phi
-    x = RAYON * lmbd - seine_et_marne.bbox[0]
-    y = y_max - RAYON * math.log(math.tan(math.pi / 4 + phi / 2))
-    pts_seine_et_marne[i] = (x, y)
+
+def creation_depart(shape):
+    pts = shape.points
+    out = []
+    for i in range(len(pts)):
+        lmbd = pts[i][0] * (math.pi / 180)  # lambda
+        phi = pts[i][1] * (math.pi / 180)  # phi
+        x = RAYON * lmbd - seine_et_marne.bbox[0]
+        y = y_max - RAYON * math.log(math.tan(math.pi / 4 + phi / 2))
+        out.append((x, y))
+    return out
+
 
 fltk.cree_fenetre(LONG_FENETRE, LAR_FENETRE)
-fltk.polygone(pts_seine_et_marne, tag="seine")
+for i in range(102):
+    depart = sf.shape(i)
+    pts = creation_depart(sf.shape(i))
+    fltk.polygone(pts, tag="departement" + str(i))
+    fltk.deplace("departement" + str(i), 300, 2325)
 # coords = fltk.coordonnees_objet("seine")
 # print(coords)
 # offset_x = -1 * min(x[0] for x in coords)
 # offset_y = -1 * min(y[1] for y in coords)
 # fltk.deplace("seine", offset_x, offset_y)
-fltk.deplace('seine', 0, 4800)
 fltk.attend_ev()
 fltk.ferme_fenetre()
